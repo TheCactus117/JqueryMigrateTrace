68513ff8fc38737089c2eba86bc288d58df7d5d3
Kévin Pignot <kevin.pignot@live.fr>
2021-08-22 18:02:34 +0200

    Init module

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    paths: {
+        "mute-migrate": "TheCactus117_JqueryMigrateTrace/js/mute-migrate"
+    },
+    shim: {
+        'jquery/jquery-migrate': ['jquery', 'mute-migrate']
+    }
+}
